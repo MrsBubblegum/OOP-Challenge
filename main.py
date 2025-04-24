5e57d7745d74b74dbced733df42b32835843e80d
Olusanmi Pamilerin kehinde <pamilerinolusanmi@gmail.com>
2025-04-24 12:49:20 +0100

    made corrections to the main and pet files

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+# Create a pet
+my_pet = Pet("Fido")
+
+# Interact with the pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.train("roll over")
+my_pet.train("fetch")
+my_pet.show_tricks()
+my_pet.get_status()
